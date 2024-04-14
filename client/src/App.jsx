285f9bc1a18865d43c3ee3b078b699cbc2797d26
Thunderer2106 <114282536+Thunderer2106@users.noreply.github.com>
2024-04-14 16:39:45 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,11 @@ import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
-
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
