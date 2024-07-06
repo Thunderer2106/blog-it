5b45e10a113d8901a5762f602c8f5fd789e206ec
Thunderer2106 <114282536+Thunderer2106@users.noreply.github.com>
2024-07-06 22:59:33 +0530

    update user functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -10,14 +10,25 @@ import {
 import { CircularProgressbar } from "react-circular-progressbar";
 import { Alert } from "flowbite-react";
 import { app } from "../firebase";
+import {
+  updateFailure,
+  updateStart,
+  updateSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [image, setimage] = useState(null);
   const [imgurl, setimgurl] = useState(null);
   const [ImageFileUploadProgress, setImageFileUploadProgress] = useState(null);
   const [ImageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [formData, setFormData] = useState({});
   const filepickerref = useRef();
-  console.log(ImageFileUploadProgress, ImageFileUploadError);
+  const dispatch = useDispatch();
+  // console.log(ImageFileUploadProgress, ImageFileUploadError);
   const handleimageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -25,13 +36,74 @@ const DashProfile = () => {
       setimgurl(URL.createObjectURL(file));
     }
   };
-  // console.log(imgurl);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (Object.keys(formData).length === 0) {
+  //     return;
+  //   }
+  //   try {
+  //     dispatch(updateStart());
+  //     const res = await fetch(`/api/user/update/${currentUser._id}`, {
+  //       method: "PUT",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+  //     const data = res.json();
+  //     if (!res.ok) {
+  //       dispatch(updateFailure(data.message));
+  //     } else {
+  //       dispatch(updateSuccess(data));
+  //     }
+  //   } catch (err) {
+  //     dispatch(updateFailure(err.message));
+  //   }
+  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError("No changes made");
+      return;
+    }
+    if (imageFileUploading) {
+      setUpdateUserError("Please wait for image to upload");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
+    }
+  };
   useEffect(() => {
     if (image) {
       uploadImage();
     }
   }, [image]);
   const uploadImage = async () => {
+    setImageFileUploading(true);
     setImageFileUploadError(null);
 
     const storage = getStorage(app);
@@ -53,13 +125,13 @@ const DashProfile = () => {
         setImageFileUploadProgress(null);
         setimage(null);
         setimgurl(null);
-        //     // setImageFileUploading(false);
+            setImageFileUploading(false);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           setimgurl(downloadURL);
-          // setFormData({ ...formData, profilePicture: downloadURL });
-          // setImageFileUploading(false);
+          setFormData({ ...formData, profilePicture: downloadURL });
+          setImageFileUploading(false);
         });
       }
     );
@@ -67,7 +139,7 @@ const DashProfile = () => {
   return (
     <div className=" max-w-lg w-full mx-auto p-4 ">
       <h1 className=" font-medium text-4xl text-center ">Profile</h1>
-      <form className="flex flex-col gap-5 pt-5 mt-10 ">
+      <form className="flex flex-col gap-5 pt-5 mt-10 " onSubmit={handleSubmit}>
         <input
           type="file"
           accept="image/*"
@@ -114,17 +186,20 @@ const DashProfile = () => {
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
+          onChange={handleChange}
         ></TextInput>
         <TextInput
           type="email"
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         ></TextInput>
         <TextInput
           type="password"
           id="password"
           placeholder="password"
+          onChange={handleChange}
         ></TextInput>
         <Button type="submit" gradientDuoTone="cyanToBlue" outline>
           {" "}
@@ -135,6 +210,16 @@ const DashProfile = () => {
         <span className="cursor-pointer"> Delete Account</span>
         <span className="cursor-pointer"> Sign Out</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color="success" className="mt-5">
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color="failure" className="mt-5">
+          {updateUserError}
+        </Alert>
+      )}
     </div>
   );
 };
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,8 +23,28 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.currentUser = action.payload;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInFailure, signInStart, signInSuccess } = userSlice.actions;
+export const {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 export default userSlice.reducer;
